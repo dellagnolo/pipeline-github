f8c1608e336cf826f2500233ca79b12fe3cd7f8c
Angelo <angelo.junior@paytrack.com.br>
2023-05-04 16:36:32 -0300

    teste

--- main.tf
+++ main.tf
@@ -3,36 +3,11 @@ terraform {
   required_version = ">= 1.0.0"
 
   required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "2.94.0"
-    }
-
-    aws = {
+    
+       aws = {
       source  = "hashicorp/aws"
       version = "3.73.0"
     }
-  }
-
-  backend "azurerm" {
-    resource_group_name  = "remote-state"
-    storage_account_name = "danielgilremotestate"
-    container_name       = "remote-state"
-    key                  = "pipeline-gitlab-ci/terraform.tfstate"
-  }
-}
-
-provider "azurerm" {
-  features {}
-}
-
-data "terraform_remote_state" "vnet" {
-  backend = "azurerm"
-  config = {
-    resource_group_name  = "remote-state"
-    storage_account_name = "danielgilremotestate"
-    container_name       = "remote-state"
-    key                  = "azure-vnet/terraform.tfstate"
   }
 }
 
